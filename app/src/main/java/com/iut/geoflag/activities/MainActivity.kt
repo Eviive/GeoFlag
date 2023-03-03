56781f4892c5478e5876d4cdcc38731ba5c36140
Eviive <albert.vaillon21@gmail.com>
2023-03-03 12:50:39 +0100

    feat: check internet connection

--- app/src/main/java/com/iut/geoflag/activities/MainActivity.kt
+++ app/src/main/java/com/iut/geoflag/activities/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.iut.geoflag.activities
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +15,7 @@ import com.iut.geoflag.api.RetrofitHelper
 import com.iut.geoflag.databinding.ActivityMainBinding
 import com.iut.geoflag.fragments.HomeFragment
 import com.iut.geoflag.fragments.MapFragment
+import com.iut.geoflag.fragments.NoConnectionFragment
 import com.iut.geoflag.fragments.QuizFragment
 import com.iut.geoflag.models.Country
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -22,41 +27,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private var currentTab = 1
+    private var loadedData = false
+
     private val countries = ArrayList<Country>()
 
-    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val countriesAPI = RetrofitHelper.getInstance().create(CountriesAPI::class.java)
-
-        GlobalScope.launch {
-            val response = countriesAPI.getCountries()
-
-            if (response.isSuccessful) {
-                val result = response.body()
-
-                if (result != null) {
-                    countries.addAll(result)
-                    loadFragment(HomeFragment(countries), 1)
-
-                    Log.i("MainActivity", "Number of countries: ${result.size}")
-
-                    for (country in result) {
-                        Log.i("MainActivity", country.name.official)
-                    }
-                }
-            } else {
-                Log.e("MainActivity", response.errorBody().toString())
-                Snackbar.make(binding.root, "Error fetching countries", Snackbar.LENGTH_SHORT).show()
-            }
+        if (checkForInternet()) {
+            loadCountries()
+        } else {
+            loadFragment(NoConnectionFragment(), 1)
+            setupNetworkCallback()
         }
 
         binding.navigationView.setOnItemSelectedListener {
-            if (currentTab == it.itemId) {
+            if (!loadedData || currentTab == it.itemId) {
                 return@setOnItemSelectedListener false
             }
             when (it.itemId) {
@@ -76,6 +65,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun loadCountries() {
+        val countriesAPI = RetrofitHelper.getInstance().create(CountriesAPI::class.java)
+
+        GlobalScope.launch {
+            val response = countriesAPI.getCountries()
+
+            if (response.isSuccessful) {
+                val result = response.body()
+
+                if (result != null) {
+                    countries.addAll(result)
+                    loadedData = true
+                    loadFragment(HomeFragment(countries), 1)
+
+                    Log.i("MainActivity", "Number of countries: ${result.size}")
+
+                    for (country in result) {
+                        Log.i("MainActivity", country.name.official)
+                    }
+                }
+            } else {
+                Log.e("MainActivity", response.errorBody().toString())
+                Snackbar.make(binding.root, "Error fetching countries", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun loadFragment(fragment: Fragment, index: Int){
         val transaction = supportFragmentManager.beginTransaction()
 
@@ -83,12 +100,49 @@ class MainActivity : AppCompatActivity() {
             transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
         } else if (currentTab > index) {
             transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+        } else {
+            transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
         }
 
         currentTab = index
 
         transaction.replace(binding.fragmentContainer.id, fragment)
         transaction.commit()
+    }
+
+    private fun checkForInternet(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val network = connectivityManager.activeNetwork ?: return false
+
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
+
+        // combiner les 2 techniques pour check internet
+        // vérifier une première fois si le téléphone est connecté à internet :
+        //     si oui load les données
+        //     si non afficher le fragment NoConnectionFragment
+        //            setup le network callback pour load les données quand le téléphone sera connecté à internet
+    }
+
+    private fun setupNetworkCallback() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                if (!loadedData) {
+                    loadCountries()
+                }
+            }
+        }
+
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
     }
 
 }
--- app/src/main/java/com/iut/geoflag/fragments/MapFragment.kt
+++ app/src/main/java/com/iut/geoflag/fragments/MapFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.iut.geoflag.databinding.FragmentMapBinding
 
-class MapFragment : Fragment() {
+class MapFragment: Fragment() {
 
     private lateinit var binding: FragmentMapBinding
 
